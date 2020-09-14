cd661668423f280588fba02c57b3aa708c0655de
treasure <929679517@qq.com>
2020-09-14 20:35:17 +0800

    update my address

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/zhengqi12/f37c7764b5c24b7aa1586f967f197ab2?v=5cc3ef35587242679bc66d75d1adf1a3"
+                configTable: "https://www.notion.so/zhengqi12/566a2723d9584c709d89e15a3cdc238d?v=da1bacfeaf5b4fe78177822ada83222f"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/zhengqi12/7ff116f77971446eb884e1ebe40c4a4a?v=1ae033c2deaf4f51b057018937664722"
+                configTable: "https://www.notion.so/zhengqi12/4b03f248e4e74429bcbe8a0e8d89adce?v=69645962d7114015bd8fd193ec9d02c0"
             }
         }
     ],
